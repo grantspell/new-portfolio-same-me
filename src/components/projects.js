048b11eba1374cfe1da90388f5985a2948d9b768
grantspell <spellgrant@gmail.com>
2018-02-22 20:52:12 -0500

    UPDATED | projects` descriptions

--- src/components/projects.js
+++ src/components/projects.js
@@ -83,14 +83,14 @@ class Projects extends Component {
         dilettante: {
             name: "Dilettante",
             imgURL: "https://i.imgur.com/6ZmsZos.png",
-            link: "http://project-four.heroku.com",
-            description: "Et quinta decima eodem modo typi qui nunc nobis videntur parum clari fiant sollemnes. Adipiscing elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Erat volutpat ut wisi enim ad minim, veniam quis nostrud exerci. In iis qui facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Dignissim qui blandit praesent luptatum zzril delenit augue duis dolore, te feugait nulla facilisi nam."
+            link: "https://project-four.herokuapp.com/",
+            description: "Dilettante is an art fusion application designed to allow users to create vision/mood boards that pair visual and audio art pieces. The user can also create an entry for each collection that can be used for research, self-expression or just for fun. Built primarily using React on Rails and PostgreSQL, this application has powerful functionality. Using additional tools such as iTunes Search API and Axios allowed me to incorporate large amounts of data without weighing down my server."
         },
         hangman: {
             name: "Hangman",
             imgURL: "https://i.imgur.com/8WZPVls.png",
-            link: "http://gspell.com",
-            description: "Et quinta decima eodem modo typi qui nunc nobis videntur parum clari fiant sollemnes. Adipiscing elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Erat volutpat ut wisi enim ad minim, veniam quis nostrud exerci. In iis qui facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Dignissim qui blandit praesent luptatum zzril delenit augue duis dolore, te feugait nulla facilisi nam."
+            link: "http://styx-hangman.bitballoon.com",
+            description: "This interactive hangman application was built using HTML, CSS and vanilla JavaScript. It is themed around the rock anthem 'Renegade' by Styx. Abiding by the simple rules of hangman, the player guesses, letter by letter, what the hidden word is for each round. All of the game's graphics were designed by hand and then illustrated using 'Affinity Photo', an iPad application. The biggest struggles with this project were the logic and the hangman animation. I used several if statements to compare the player's guess to each letter in the hidden word chosen for the round. I used if statements for the animation as well, telling the application to render certain images according to the player's incorrect guesses."
         }
     }
 
@@ -122,10 +122,10 @@ class Projects extends Component {
 
                     <Buttons>
                         <div className="button">
-                            <a href="http://project-four.heroku.com">PUBLISHED</a>
+                            <a href="https://project-four.herokuapp.com/" target="_blank" >PUBLISHED</a>
                         </div>
                         <div className="button">
-                            <a href="">GITHUB</a>
+                            <a href="https://github.com/grantspell/project-four" target="_blank" >GITHUB</a>
                         </div>
                     </Buttons>
                 </Project>
@@ -142,22 +142,20 @@ class Projects extends Component {
                             <p>{this.state.hangman.description}</p>
                             <h3>TECHNOLOGIES USED</h3>
                             <ul>
-                                <li>Ruby on Rails</li>
-                                <li>ReactJS</li>
-                                <li>PostgreSQL</li>
-                                <li>Axios</li>
-                                <li>Styled-Components</li>
-                                <li>iTunes Search API</li>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                                <li>JavaScript</li>
+                                <li>jQuery</li>
                             </ul>
                         </Content>
                     </TopWrapper>
 
                     <Buttons>
                         <div className="button">
-                            <a href="">PUBLISHED</a>
+                            <a href="http://styx-hangman.bitballoon.com" target="_blank" >PUBLISHED</a>
                         </div>
                         <div className="button">
-                            <a href="">GITHUB</a>
+                            <a href="https://github.com/grantspell/project_one" target="_blank" >GITHUB</a>
                         </div>
                     </Buttons>
                 </Project>
